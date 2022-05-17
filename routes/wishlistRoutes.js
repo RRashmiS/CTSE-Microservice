7dea4760b47e04db7f3d5932d62ea7e51f655ade
Rashmi Raveena <it19048642@my.sliit.lk>
2022-05-17 23:35:07 +0530

    delete wishlist item

--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -25,5 +25,34 @@ router.route("/getAll").get((req,res)=>{
     })
 })
 
+router.route("/update/:id").put(async(req,res) =>{
+    let userId = req.body.id;
+    const {name,price,description,stockAvailable,brandName,category} = req.body;
+
+    const updateProduct = {
+        name,
+        price,
+        description,
+        stockAvailable,
+        brandName,
+        category
+    }
+
+    const update = await Product.findByIdAndUpdate(userId,updateProduct)
+    .then(() => {
+        res.status(200).send({status: "Product Updated",user: update})
+    }).catch((error)=>{
+    res.status(500).send({status: "Error with update"})
+    })
+})
+router.route("/delete/:id").delete(async (req,res) => {
+    let userId = req.body.id;
+    await Product.findByIdAndDelete(userId)
+    .then(()=>{
+        res.status(200).send({status: "Product Deleted"})
+    }).catch((error)=>{
+        res.status(500).send({status: "Error with delete"})
+    })
+})
 
 module.exports= router
